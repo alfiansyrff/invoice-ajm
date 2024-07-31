341dfa12edd2187fcf77aebaad7712b646084452
Muhammad Nur Alfian Syarif <222112218@stis.ac.id>
2024-07-31 19:25:08 +0700

    fix button preview

--- app/components/form.js
+++ app/components/form.js
@@ -118,7 +118,7 @@ function BroadCastForm() {
           </select>
         </div>
 
-        <button type="button" onClick={extract} className="md:absolute bottom-5 right-5 mt-4 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Preview</button>
+        <button type="button" onClick={extract} className="mt-4 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Preview</button>
       </form>
     </div>
   );
